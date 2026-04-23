6782da693a2be246ad77cefafc62e0f8f4b96f14
‘mohsenpourzamany’ <pourzamany2020@gmail.com>
2026-04-24 00:14:39 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-center text-4xl font-bold mb-8 text-blue-400">
+          Operino AI
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
